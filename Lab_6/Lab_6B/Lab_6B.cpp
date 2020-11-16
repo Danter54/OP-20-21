6f2b5466bafaea930a4fe87ec40c46f90ca91ae0
Artem Danilevskiy <danilevskij77@gmail.com>
2020-11-16 15:16:10 +0200

    Add Lab_6B

---
+++ Lab_6/Lab_6B/Lab_6B.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <ctime>
+
+int main()
+{
+	setlocale(LC_ALL,"ru");
+	srand(time(NULL));
+
+	int n = 0, num1=0, num2=0, temp=0, answer=0;
+	double mark = 0;
+	printf("Введите количество примеров: ");
+	scanf("%d",&n);
+	printf("Тест начался:\n");
+
+	for (int i = 0; i < n; i++) {
+		num1 = rand() % 9 + 1;
+		num2 = rand() % 9 + 1;
+		temp = num1 * num2;
+		printf("%d * %d = ",num1,num2);
+		scanf("%d",&answer);
+
+		if (temp == answer) mark++;
+	}
+
+	printf("Количество правильных ответов: %0.0lf\n", mark);
+
+	mark =(mark/n) * 12;
+
+	printf("Твоя оценка = %0.0lf\n",mark);
+}
