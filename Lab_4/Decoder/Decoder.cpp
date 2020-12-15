46c3b779dd19333f9181a5eb0bc94cbf0bdd14a9
Artem Danilevskiy <danilevskij77@gmail.com>
2020-12-15 02:17:03 +0200

    Add Lab4

---
+++ Lab_4/Decoder/Decoder.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main(void)
+{
+	setlocale(LC_ALL, "Ru");
+	unsigned short t;
+	unsigned short d;
+	unsigned short n;
+	unsigned short int UnitStateWord;
+
+	printf("Введите слово состояния устройства(0xFFFF): ");
+	scanf_s("%hx", &UnitStateWord);
+	t = (UnitStateWord >> 10) & 0x3B;
+	d = (UnitStateWord >> 9) & 1;
+	n = UnitStateWord & 0x1FF;
+	printf("\nТип сдвига: %d\n",t);
+	printf("Направление сдвига: %d\n",d);
+	printf("Количество разрядов сдвига: %d",n);
+}
---
+++ Lab_4/Lab_4/Lab_4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main(void) {
+	setlocale(LC_ALL, "Ru");
+	unsigned short t;
+	unsigned short d;
+	unsigned short n;
+	unsigned short int UnitStateWord;
+
+	printf("Введите тип сдвига(0...3): ");
+	scanf_s("%hu", &t);
+	printf("Введите направление сдвига(0...1): ");
+	scanf_s("%hu", &d);
+	printf("Введите количество разрядов сдвига(0...511):");
+	scanf_s("%hu", &n);
+
+	UnitStateWord = (t & 0x37) << 10;
+	UnitStateWord |= (d & 1) << 9;
+	UnitStateWord |= n & 0x1FF;
+	printf("\nСлово состояния устройства = %04x\n",
+		UnitStateWord);
+	return 0;
+}
